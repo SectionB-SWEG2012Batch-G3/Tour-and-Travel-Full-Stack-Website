e58d81e74539862a7d6d8c678b3c3cc2d7154836
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-05-16 00:16:02 +0300

    trip form validation

--- tourguide/saveAssignedTourGuides.php
+++ tourguide/saveAssignedTourGuides.php
@@ -1,15 +1,23 @@
 <?php
     include_once '../dbconfig/connection.php';
+    include_once 'partials/test_input.php';
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $name = '';
-        $place = '';
-        $tele = '';
-        $email = '';
-        $guideName = '';
-        $stDate = '';
-        $enDate = '';
-        $price = '';
-        $cardNum = '';
+        $stmt = $pdo->prepare("SELECT title FROM places_to_visit");
+        $stmt->execute();
+        $places_to_visit = $stmt->fetchAll();
+        $stmt = $pdo->prepare("SELECT name FROM tourguide");
+        $stmt->execute();
+        $guides = $stmt->fetchAll();
+        $name = test_input($_POST['Fname'] ?? '');
+        $place = test_input($_POST['place'] ?? '');
+        $tele = test_input($_POST['mobile'] ?? '');
+        $email = test_input($_POST['E-mail'] ?? '');
+        $guideName = test_input($_POST['guides'] ?? '');
+        $stDate = test_input($_POST['Date-st'] ?? '');
+        $enDate = test_input($_POST['Date-en'] ??'');
+        $pricePerH = test_input($_POST['perHour'] ?? '');
+        $price = test_input($_POST['price'] ?? '');
+        $cardNum = test_input($_POST['credit-card'] ?? '');
         $nameErr = '';
         $placeErr = '';
         $teleErr = '';
@@ -17,21 +25,94 @@
         $guideNameErr = '';
         $stDateErr = '';
         $enDateErr = '';
+        $perHourErr = '';
         $priceErr = '';
         $cardNumErr = '';
         $errors = false;
-        if (!$errors) {
-            $stmt = $pdo->prepare("INSERT INTO assigned_tourguide(traveller, place, tele, email, start_date, end_date, guide_name, price, credit_card_num) VALUES (:traveller, :place, :tele, :email, :start_date, :end_date, :guide_name, :price, :credit_card_num)");
-            $stmt->execute([
-                ':traveller' => $name,
-                ':place' => $place,
-                ':tele' => $tele,
-                ':email' => $email,
-                ':start_date' => $stDate,
-                ':end_date' => $enDate,
-                ':guide_name' => $guideName,
-                ':price' => $price,
-                ':credit_card_num' => $cardNum
-            ]);
+        #validating name of traveller
+        filter_var($name, FILTER_SANITIZE_SPECIAL_CHARS);
+        filter_var($name, FILTER_SANITIZE_STRING);
+        if (empty($name)) {
+            $nameErr[] = 'Name field can\'t be empty';
         }
+        if (!filter_var($name, FILTER_VALIDATE_EMAIL)) {
+            $nameErr[] = 'Email is  not valid';
+        }
+        # validarting place
+        filter_var($place, FILTER_SANITIZE_SPECIAL_CHARS);
+        filter_var($place, FILTER_SANITIZE_STRING);
+        if (!in_array($place, $places_to_visit)) {
+            $placeErr[] = 'Place name doesn\'t exist';
+        }
+        if (empty($place)) {
+            $placeErr[] = 'Name field can\'t be empty';
+        }
+
+        if (strlen($place) > 30) {
+            $placeErr[] = 'Place name is too long';
+        }
+
+        # validating phone number
+        if (empty($tele)) {
+            $teleErr[] = 'Name field can\'t be empty';
+        }
+
+        if (!preg_match('/^(\+251|0)([\s-]?9\d{1}|(9\d{1}))[\s-]?\d{3}[\s-]?\d{4}$/', $tele)) {
+            $teleErr[] = 'phone number doesn\'t match the pattern';
+        }
+
+        #validating guidename
+        filter_var($guideName, FILTER_SANITIZE_SPECIAL_CHARS);
+        if (empty($guideName)) {
+            $guideNameErr[] = 'Please select tourguide name';
+        }
+    
+        if (!in_array($guideName, $guides)) {
+            $guideNameErr[] = 'The tour guide is doesn\'t  exist';
+        }
+        if (strlen($guideName) > 30) {
+            $guideNameErr[] = 'Tourguide name  is too long';
+        }
+
+        #validating email
+        filter_id($email, FILTER_SANITIZE_EMAIL);
+        if (empty($email)) {
+            $emailErr[] = 'Email is requied';
+        }
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr[] = 'Email is not valid';
+        }
+        if (strlen($email) > 30) {
+            $emailErr[] = 'Email is too long';
+        }
+        #validating price
+        filter_id($pricePerH, FILTER_SANITIZE_NUMBER_FLOAT);
+        if (empty($pricePerH)) {
+            $perHourErr[] = 'price per hour empty please select tourguide';
+        }
+        filter_id($price, FILTER_SANITIZE_NUMBER_FLOAT);
+        filter_id($cardNum, FILTER_SANITIZE_NUMBER_INT);
+        if (empty($cardNum)) {
+            $cardNumErr[] = 'Please inset your creadit card number';
+        }
+        if ($cardNum <= 100000) {
+            $cardNumErr[] = 'Credit card number should have at least 6 chars';
+        }
+
+        echo '<br/>'.$name.'<br/>'.$place.'<br/>'.$tele.'<br/>'.$email.'<br/>'.$guideName.'<br/>'.$pricePerH.'<br/>'.$price.'<br/>'.$cardNum.'<br/>'.$tele.'<br/>';
+
+        // if (!$errors) {
+        //     $stmt = $pdo->prepare("INSERT INTO assigned_tourguide(traveller, place, tele, email, start_date, end_date, guide_name, price, credit_card_num) VALUES (:traveller, :place, :tele, :email, :start_date, :end_date, :guide_name, :price, :credit_card_num)");
+        //     $stmt->execute([
+        //         ':traveller' => $name,
+        //         ':place' => $place,
+        //         ':tele' => $tele,
+        //         ':email' => $email,
+        //         ':start_date' => $stDate,
+        //         ':end_date' => $enDate,
+        //         ':guide_name' => $guideName,
+        //         ':price' => $price,
+        //         ':credit_card_num' => $cardNum
+        //     ]);
+        // }
     }
--- tourguide/tripForm.php
+++ tourguide/tripForm.php
@@ -1,7 +1,7 @@
 <?php
     include_once '../dbconfig/connection.php';
     $name = '';
-    if(isset($_GET['name'])){
+    if (isset($_GET['name'])) {
         $name = $_GET['name'];
     }
     $stmt = $pdo->prepare("SELECT * FROM tourguide");
@@ -11,73 +11,78 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>4HF Tour and Travel|Create Trip Form</title>
     <link rel="stylesheet" href="../fontawesome-free-5.15.4-web/css/all.css">
-    <script defer src = "js/createTrip.js"></script>
-    <script defer src = "js/tripDataValidation.js"></script>
+    <script defer src="js/createTrip.js"></script>
+    <script defer src="js/tripDataValidation.js"></script>
     <link rel="stylesheet" href="css/tripCSS.css">
 </head>
 <style>
-.trip-form-container{
-    display: grid;
-    grid-template-columns: auto;   
-}
-
-    .trip-form-container fieldset{
-        width: 300px;
-        background-color: rgba(0,0,0,0.5);
+    .trip-form-container {
+        display: grid;
+        grid-template-columns: auto;
     }
 
-    .trip-form-container label{
+    .trip-form-container fieldset {
+        width: 300px;
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    .trip-form-container label {
         width: 100%;
         margin: 5px;
     }
 
-    .trip-form-container input:focus{
-        border:1px solid blue;
+    .trip-form-container input:focus {
+        border: 1px solid blue;
         line-height: 25px;
         transform: scaleX(1.2);
     }
 
-    #reset,#submit-btn{
+    #reset,
+    #submit-btn {
         display: inline;
         margin-inline: 10px;
         transform: scaleX(1.4);
     }
 </style>
+
 <body style="background-image: url('bahirdar.jpg');">
-    <section class = "trip-form-container">		
+    <section class="trip-form-container">
         <fieldset>
             <legend>Create trip form</legend>
-            <form id = "form1">
+            <form id="form1">
                 <div class="input-container">
-                    <label for = "name">Full name</label>
-                    <input type = "text" name = "Fname" class = "no-outline" id= "name" placeholder = "Full Name" maxlength = "20" required>
+                    <label for="name">Full name</label>
+                    <input type="text" name="Fname" class="no-outline" id="name" placeholder="Full Name" maxlength="20"
+                        required>
                     <i class="fas fa-check-circle"></i>
                     <i class="fas fa-exclamation-circle"></i>
                     <small></small>
                 </div>
                 <div class="input-container">
-                    <label for = "place">Where you are going to go?</label>
-                    <input type = "Search" class = "no-outline" id = "place" name = "place" value = "Addis Ababa, Ethiopia"  required>
+                    <label for="place">Where you are going to go?</label>
+                    <input type="Search" class="no-outline" id="place" name="place" value="Addis Ababa, Ethiopia"
+                        required>
                     <i class="fas fa-check-circle"></i>
                     <i class="fas fa-exclamation-circle"></i>
                     <small></small>
                 </div>
                 <div class="input-container">
                     <label for="tele">Phone </label>
-                    <input type="tel" id = "tele" name = "mobile">
+                    <input type="tel" id="tele" name="mobile">
                     <i class="fas fa-check-circle"></i>
                     <i class="fas fa-exclamation-circle"></i>
                     <small></small>
                 </div>
                 <div class="input-container">
-                    <label for = "mail">Email</label><br>
-                    <input type = "email" id = "mail" name = "E-mail" placeholder = "Username@gmail.com" required>
+                    <label for="mail">Email</label><br>
+                    <input type="email" id="mail" name="E-mail" placeholder="Username@gmail.com" required>
                     <i class="fas fa-check-circle"></i>
                     <i class="fas fa-exclamation-circle"></i>
                     <small></small>
@@ -85,47 +90,51 @@
                 <div class="input-container">
                     <label for="guides">Tour Guide Name</label><br>
                     <select name="guides" id="guides">
-                        <?php if(!empty($tourguides)):?>
-                            <?php foreach($tourguides as $i => $tourguide):?>
-                                <option value="<?php echo $tourguide['name']?>" <?php if($name === $tourguide['name']) echo 'selected'?>>
-                                    <?php 
-                                        echo $tourguide['name'];
-                                    ?> Chane 
-                                </option>
-                            <?php endforeach?>
+                        <?php if (!empty($tourguides)):?>
+                        <?php foreach ($tourguides as $i => $tourguide):?>
+                        <option
+                            value="<?php echo $tourguide['name']?>"
+                            <?php if ($name === $tourguide['name']) {
+    echo 'selected';
+}?>>
+                            <?php
+                                echo $tourguide['name'];
+                            ?> Chane
+                        </option>
+                        <?php endforeach?>
                         <?php endif?>
-                       
+
                     </select>
                     <i class="fas fa-check-circle"></i>
                     <i class="fas fa-exclamation-circle"></i>
                     <small></small>
-                </div>  
+                </div>
                 <div class="input-container">
-                    <label for = "Date">From date</label>
-                    <input type = "date" id = "St-Date" name = "Date-st" required>
-                    <i style = "position: absolute;right: 80px;top:38px" class="fas fa-check-circle"></i>
-                    <i style = "position: absolute;right: 80px;top:38px" class="fas fa-exclamation-circle"></i>
+                    <label for="Date">From date</label>
+                    <input type="datetime-local" id="St-Date" name="Date-st" required>
+                    <i style="position: absolute;right: 80px;top:38px" class="fas fa-check-circle"></i>
+                    <i style="position: absolute;right: 80px;top:38px" class="fas fa-exclamation-circle"></i>
                     <small></small>
                 </div>
                 <div class="input-container">
-                    <label for = "Date">To date</label> 
-                    <input type = "date" id = "En-Date" name = "Date-en" required>
-                    <i style = "position: absolute;right: 80px;top:38px" class="fas fa-check-circle"></i>
-                    <i style = "position: absolute;right: 80px;top:38px" class="fas fa-exclamation-circle"></i>
+                    <label for="Date">To date</label>
+                    <input type="datetime-local" id="En-Date" name="Date-en" required>
+                    <i style="position: absolute;right: 80px;top:38px" class="fas fa-check-circle"></i>
+                    <i style="position: absolute;right: 80px;top:38px" class="fas fa-exclamation-circle"></i>
                     <small></small>
                 </div>
                 <div class="input-container">
                     <label for="credit-card">Credit card number</label>
-                    <input type="password" name  = "credit-card" id = "credit-card">
-                    <i class="fas fa-eye" id = "on" ></i>
-				    <i class="fas fa-eye-slash" id = "off"></i>
+                    <input type="password" name="credit-card" id="credit-card">
+                    <i class="fas fa-eye" id="on"></i>
+                    <i class="fas fa-eye-slash" id="off"></i>
                     <i class="fas fa-check-circle"></i>
                     <i class="fas fa-exclamation-circle"></i>
                     <small></small>
                 </div>
                 <div class="input-container">
-                    <label for="perhour" >Price/Hr.</label>
-                    <input type="text" id= "perhour" disabled>
+                    <label for="perhour">Price/Hr.</label>
+                    <input type="text" id="perhour" name="perHour" disabled>
                     <i class="fas fa-check-circle"></i>
                     <i class="fas fa-exclamation-circle"></i>
                     <small></small>
@@ -137,12 +146,13 @@
                     <i class="fas fa-exclamation-circle"></i>
                     <small></small>
                 </div>
-            
-                <input type = "submit" id = "submit-btn" name = "submit" value = "Create">
-                <input style = "transform: translateX(10px);" type = "reset" id = "reset" name = "clear" value = "clear">
+
+                <input type="submit" id="submit-btn" name="submit" value="Create">
+                <input style="transform: translateX(10px);" type="reset" id="reset" name="clear" value="clear">
             </form>
-            </fieldset>
-</section>
-<?php include_once 'partials/frontEndValidation.php';?>
+        </fieldset>
+    </section>
+    <?php include_once 'partials/frontEndValidation.php';?>
 </body>
+
 </html>
